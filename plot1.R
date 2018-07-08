4f2e3b1d81c68c117c122cbed04398d7eb5ca995
mfukuzawa <mfukuzawa69@gmail.com>
2018-07-08 14:50:14 -0500

    plot1 code

---
+++ plot1.R
@@ -0,0 +1,32 @@
+### Exploratory Data Analysis, Week1
+
+## Load data (set wd to data location)
+data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?",
+                   colClasses = c("character","character","numeric","numeric","numeric",
+                                  "numeric","numeric","numeric","numeric"))
+
+## Date column change to Date type; current format is d/m/y
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+
+## Time column change type
+#data$Time <- as.POSIXct(data$Time, format = "%H:%M:%s")
+
+## Subset only dates we want
+dataSubset <- subset(data, data$Date == '2007-02-01' | data$Date == '2007-02-02')
+
+## Combine Date and Time columns into one so that we can use POSIXlt for days of week
+DateTime <- paste(dataSubset$Date, dataSubset$Time)
+
+## Now convert DateTime column
+dataSubset$DateTime <- as.POSIXlt(DateTime)
+
+## Reorder data file to include new DateTime, exclude original Date and Time
+dataSubset <- dataSubset[,c(10,3:9)]
+
+######################################
+
+## Create first plot
+png(filename = "plot1.png", width = 480, height = 480)
+hist(dataSubset$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)",
+     col = "red")
+dev.off()
